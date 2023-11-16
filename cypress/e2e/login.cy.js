b617c103675cb92419f165bb6524f530017638e3
hasan627 <hasan420032@gmail.com>
2023-11-16 18:27:06 +0600

    Link Update

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,9 +5,9 @@ const loginpage = new LoginPage();
 describe('LoginTest', () => {
   it('login', () => {
     cy.viewport(1400, 750)
-    cy.visit('/login')
+    cy.visit('')
     cy.get('.header-right > .MuiList-root > :nth-child(6) > a').click()
-    cy.visit('http://192.168.30.61:8090/login')
+    //cy.visit('http://192.168.30.61:8090/login')
     loginpage.UserName().type('admin')
     loginpage.UserPassword().type('admin')
     loginpage.ClickLogin() 
